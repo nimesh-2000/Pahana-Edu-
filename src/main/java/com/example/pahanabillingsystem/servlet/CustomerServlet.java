5c686e2d6437de22ec2ef3a64005141bb261c428
Nimesh Denuwana <nimeshdenuwan2000@gmail.com>
2025-08-11 11:43:13 +0530

    f

---
+++ src/main/java/com/example/pahanabillingsystem/servlet/CustomerServlet.java
@@ -0,0 +1,105 @@
+package com.example.pahanabillingsystem.servlet;
+
+import com.example.pahanabillingsystem.model.Customer;
+import com.example.pahanabillingsystem.service.CustomerService;
+import com.example.pahanabillingsystem.service.UserService;
+import com.example.pahanabillingsystem.service.impl.CustomerServiceImpl;
+import com.example.pahanabillingsystem.service.impl.UserServiceImpl;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
+
+    private final CustomerService customerService = new CustomerServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        createCustomer(request, response);
+    }
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        updateCustomer(request, response);
+    }
+
+
+         /*----------------------------------------------------------------------*/
+    private void createCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String accountNumber = request.getParameter("accountNumber");
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String telephone = request.getParameter("telephone");
+
+
+        Customer customer = new Customer(0, accountNumber, name, address, telephone);
+
+        // Call service to add car and populate the carDTO with the ID
+        customerService.addCustomer(customer);
+        int customerId = customer.getId();  // Get the ID of the newly added car
+
+
+        sendSuccessResponse(response, "Car and Image added successfully.");
+    }
+
+    private void updateCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String customerIdParam = request.getParameter("customerId");
+
+
+        if (customerIdParam == null || customerIdParam.isEmpty()) {
+            sendErrorResponse(response, "Customer ID is missing.");
+            return;
+        }
+
+        int customerId = Integer.parseInt(customerIdParam);
+
+        Customer customer = customerService.getCustomerById(customerId);
+
+        if (customer != null) {
+            // Update car details
+            customer.setAccountNumber(request.getParameter("accountNumber"));
+            customer.setName(request.getParameter("name"));
+            customer.setAddress(request.getParameter("address"));
+            customer.setTelephone(request.getParameter("telephone"));
+
+
+
+            // Call service to update car
+            customerService.updateCustomer(customer);
+
+            sendSuccessResponse(response, "Car updated successfully.");
+        } else {
+            sendErrorResponse(response, "Car not found.");
+        }
+    }
+
+
+
+
+
+
+    private void sendSuccessResponse(HttpServletResponse response, String message) throws IOException {
+        response.setContentType("application/json");
+        response.getWriter().write(new Gson().toJson(new ResponseMessage(message)));
+    }
+    private void sendErrorResponse(HttpServletResponse response, String message) throws IOException {
+        response.setContentType("application/json");
+        response.getWriter().write(new Gson().toJson(new ResponseMessage(message)));
+    }
+
+    private static class ResponseMessage {
+        private String message;
+
+        public ResponseMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
